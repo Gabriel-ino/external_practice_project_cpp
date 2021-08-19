d7295c4cc99725b2ad0940055a7d83558b830541
Gabriel <60673704+Gabriel-ino@users.noreply.github.com>
2021-08-18 21:00:39 -0300

    Update foo.cpp

--- foo.cpp
+++ foo.cpp
@@ -1,1 +1,18 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+extern string hello;
+extern int cont;
+
+void write_extern(void){
+    cout << "Count is: " << cont << endl;
+}
+
+void foo(void){
+    extern int i;
+    i = 100;
+    hello = "World";
+}
 
